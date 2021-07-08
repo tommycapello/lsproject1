c0a9c1e50d439ced0502062d02716b755e112623
tommycapello <thomcapello@gmail.com>
2021-07-08 16:34:34 -0400

    wip

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ if(process.env.NODE_ENV === 'development'){
     server.use(cors())
 }
 
+server.use('*', (req,res) => {
+    res.send('<h1>What up!!</h1>')
+})
+
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
